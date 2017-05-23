30b00c3475cefd5f06ae3caf67e7b7678d0d09db
maria <mariabrown@Marias-MacBook.fios-router.home>
2017-05-22 19:42:56 -0500

    added photo

--- apps/main/views.py
+++ apps/main/views.py
@@ -21,7 +21,8 @@ def index(request, n):
     'http://www.vfmii.com/medlib6/VIZ_C4415490_photo/0/69/721/69721854/69721854.jpg',
     'http://1adt.com/wp-content/uploads/2015/06/most-breathtaking-tropical-photos-and-places-in-the-world-houses-on-the-beach-1adt.com_.jpgd
     'https://s-media-cache-ak0.pinimg.com/originals/4f/a3/6d/4fa36d3b93bd3f8d3971a48ed727e5a6.jpg'
-    'http://eskipaper.com/images/stunning-tropical-wallpaper-1.jpg'
+    'http://eskipaper.com/images/stunning-tropical-wallpaper-1.jpg',
+    'http://www.technocrazed.com/wp-content/uploads/2016/04/Bora-Bora-Island_French-Polynesia_FO-HB9STJ_DX-News.jpg',
     ]
     if n < 1 or n > 50:
         pic = random.choice(landscapes)
